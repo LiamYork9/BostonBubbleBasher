dfd0b087a0e2242e52d7f1e0c5e751c4ba05442a
Garrett Shipp <garwad9@gmail.com>
2025-01-25 11:53:20 -0600

    started the boss fms

---
+++ Assets/Scripts/BossFSM/BIGMANStateMachine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using StateMachine;
+
+public class BIGMANStateMachine : SM_StateMachine
+{
+    //States
+    public RandomAttack random;
+    public ConsecutiveShooting conShooting;
+    public Kick kick;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
