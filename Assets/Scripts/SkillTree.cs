09dad389ff77340d429d4d04f58f3eb91d8edb62
Christian Wiggins <Christian.wiggins0@gmail.com>
2025-01-25 00:36:54 -0600

    Skill Tree UI is mostly working

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -1,7 +1,40 @@
 using UnityEngine;
+using TMPro;
 
 public class SkillTree : MonoBehaviour
 {
-    public static SkillTree skillTree;
-    private void Awake() => skillTree = this;
+
+    public TextMeshProUGUI atkNumberText;
+    public TextMeshProUGUI defNumberText;
+    public TextMeshProUGUI speedNumberText;
+    public TextMeshProUGUI hpNumberText;
+    int atkCounter;
+    int defCounter;
+    int speedCounter;
+    int hpCounter;
+
+    public void atkButonPressed()
+    {
+        atkCounter++;
+        atkNumberText.text = "Atk" + atkCounter + "";
+    }
+
+    public void defButonPressed()
+    {
+        defCounter++;
+        defNumberText.text = "Def" + defCounter + "";
+    }
+
+    public void speedButonPressed()
+    {
+        speedCounter++;
+        speedNumberText.text = "Speed" + speedCounter + "";
+    }
+
+    public void hpButonPressed()
+    {
+        hpCounter++;
+        hpNumberText.text = "HP" + hpCounter + "";
+    }
+
 }
