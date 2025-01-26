44e637360523fceeda0e776215452ee6a9896a11
Liam York <yorkliam08@gmail.com>
2025-01-26 15:46:25 -0600

    Anim punch

--- Assets/Scripts/InstantiateEnemyAttack.cs
+++ Assets/Scripts/InstantiateEnemyAttack.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class InstantiateEnemyAttack : MonoBehaviour
 {
+    public GameObject attack;
+    public int facing = -1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,18 @@ public class InstantiateEnemyAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameObject.GetComponent<SpriteRenderer>().flipX==true)
+        {
+            facing=-1;
+        }
+        else
+        {
+            facing=1;
+        }
+    }
+
+    public void MakeHitbox()
+    {
+        Instantiate(attack,transform.position+(new Vector3(1.0f*facing,0.0f,0.0f)),transform.rotation);
     }
 }
